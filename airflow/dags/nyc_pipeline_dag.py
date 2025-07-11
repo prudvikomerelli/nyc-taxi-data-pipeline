068591b639ee9912b3a7ea16f24c99343b999aad
prudvikomerelli <51968313+prudvikomerelli@users.noreply.github.com>
2025-07-11 10:38:03 -0500

    Update nyc_pipeline_dag.py

--- airflow/dags/nyc_pipeline_dag.py
+++ airflow/dags/nyc_pipeline_dag.py
@@ -1,1 +1,24 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.utils.dates import days_ago
 
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False
+}
+
+with DAG(
+    'nyc_taxi_pipeline',
+    default_args=default_args,
+    description='ETL DAG for NYC Taxi Data',
+    schedule_interval='@daily',
+    start_date=days_ago(1),
+    catchup=False,
+) as dag:
+
+    transform = BashOperator(
+        task_id='run_spark_transform',
+        bash_command='spark-submit /opt/airflow/dags/../spark_jobs/transform_data.py'
+    )
+
+    transform
