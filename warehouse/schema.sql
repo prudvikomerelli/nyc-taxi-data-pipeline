146575609e62071d9403bab12650fcde31725b0a
prudvikomerelli <51968313+prudvikomerelli@users.noreply.github.com>
2025-07-11 14:10:39 -0500

    Update schema.sql

--- warehouse/schema.sql
+++ warehouse/schema.sql
@@ -1,1 +1,10 @@
-
+CREATE TABLE IF NOT EXISTS yellow_tripdata (
+    vendor_id TEXT,
+    pickup_datetime TIMESTAMP,
+    dropoff_datetime TIMESTAMP,
+    passenger_count INT,
+    trip_distance FLOAT,
+    rate_code TEXT,
+    payment_type TEXT,
+    total_amount FLOAT
+);
